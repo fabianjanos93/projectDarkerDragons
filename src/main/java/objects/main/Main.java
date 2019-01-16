624d72b814efc76f21ba99911334127119d3bc70
TribTrob <trib073@gmail.com>
2019-01-16 10:29:14 +0100

    Main window init

--- src/main/java/objects/main/Main.java
+++ src/main/java/objects/main/Main.java
@@ -1,7 +1,18 @@
 package objects.main;
 
+import objects.main.gui.Mainwindow;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("jej");
+
+        EventQueue.invokeLater(()->{
+            Mainwindow mainScreen = new Mainwindow();
+            mainScreen.setVisible(true);
+        });
+
     }
 }
