f9ef25361cd2ced140ef4959b6c3070b0820e5b5
Fábián János <fabian.janos93@gmail.com>
2019-01-17 14:10:33 +0100

    sprite for rat

--- src/main/java/objects/character/enemy/Rat.java
+++ src/main/java/objects/character/enemy/Rat.java
@@ -3,17 +3,25 @@ package objects.character.enemy;
 import objects.character.GameCharacter;
 import objects.skill.Skill;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class Rat extends GameCharacter {
+    private final static String sprite = "/Testimage/rat.png";
+
     public Rat(int level, Skill[] skills) {
         super(level, skills);
         this.hp = 6 + level;
         this.armor = 1 + level/2;
         this.physicalDmg = 2 + level;
+        sprite.getClass().getResource(sprite);
     }
 
     private static ArrayList<Skill> possibleSkills = new ArrayList<Skill>();
+
+    public static String getSprite() {
+        return sprite;
+    }
     public static void fillSkills(ArrayList<Skill> skills){
         possibleSkills.addAll(skills);
     }
